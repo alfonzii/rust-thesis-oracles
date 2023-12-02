c32d30898d6dfa821c7374b06f823df1c1ba1832
alfonzii <semicco2@gmail.com>
2023-12-02 18:51:31 +0100

    Added send byte array through channel functionality

--- multithread/src/main.rs
+++ multithread/src/main.rs
@@ -1,11 +1,12 @@
 mod oracle;
 mod party;
 
+use secp256kfun::{marker::*, Point, Scalar};
 use std::{sync::mpsc, thread};
 use thread_broadcaster::{BroadcastListener, Broadcaster};
 
 fn main() {
-    let (b, s1) = Broadcaster::<String>::new();
+    let (b, s1) = Broadcaster::<[u8; 33]>::new();
     let s2 = s1.clone();
 
     let oracle_thread = thread::spawn(move || {
@@ -14,24 +15,58 @@ fn main() {
 
     thread::spawn(move || {
         let ls1 = BroadcastListener::register_broadcast_listener(s1);
-        for msg in ls1.channel {
-            println!(
-                "got broadcast with data: {} on thread {:?}",
-                msg,
-                thread::current().id()
-            );
+        let mut i = 0;
+        for byte_msg in ls1.channel {
+            if i == 0 || i == 1 {
+                let statement = Point::<_, Public, NonZero>::from_bytes(byte_msg).unwrap();
+                println!(
+                    "got statement broadcast: {} on thread {:?}",
+                    statement,
+                    thread::current().id()
+                );
+                i += 1;
+            } else {
+                let mut witness_byte_arr: [u8; 32] = [0; 32];
+                get_from_bigger_arr(&byte_msg, &mut witness_byte_arr);
+                let witness_scalar = Scalar::<Secret, _>::from_bytes(witness_byte_arr).unwrap();
+                println!(
+                    "got witness broadcast: {} on thread {:?}",
+                    witness_scalar,
+                    thread::current().id()
+                );
+            }
         }
     });
     thread::spawn(move || {
         let ls2 = BroadcastListener::register_broadcast_listener(s2);
-        for msg in ls2.channel {
-            println!(
-                "got broadcast with data: {} on thread {:?}",
-                msg,
-                thread::current().id()
-            );
+        let mut i = 0;
+        for byte_msg in ls2.channel {
+            if i == 0 || i == 1 {
+                let statement = Point::<_, Public, NonZero>::from_bytes(byte_msg).unwrap();
+                println!(
+                    "got statement broadcast: {} on thread {:?}",
+                    statement,
+                    thread::current().id()
+                );
+                i += 1;
+            } else {
+                let mut witness_byte_arr: [u8; 32] = [0; 32];
+                get_from_bigger_arr(&byte_msg, &mut witness_byte_arr);
+                let witness_scalar = Scalar::<Secret, _>::from_bytes(witness_byte_arr).unwrap();
+                println!(
+                    "got witness broadcast: {} on thread {:?}",
+                    witness_scalar,
+                    thread::current().id()
+                );
+            }
         }
     });
 
     oracle_thread.join().unwrap();
+}
+
+fn get_from_bigger_arr(source: &[u8], destination: &mut [u8]) {
+    for i in 0..destination.len().min(source.len()) {
+        destination[i] = source[i];
+    }
 }
--- multithread/src/oracle.rs
+++ multithread/src/oracle.rs
@@ -12,7 +12,7 @@ use schnorr_fun::{
 use sha2::Sha256;
 use thread_broadcaster::Controller;
 
-pub fn oracle_main(broadcaster: Controller<String>) {
+pub fn oracle_main(broadcaster: Controller<[u8; 33]>) {
     let nonce_gen = nonce::Synthetic::<Sha256, nonce::GlobalRng<ThreadRng>>::default();
     let schnorr = Schnorr::<Sha256, _>::new(nonce_gen);
 
@@ -24,25 +24,43 @@ pub fn oracle_main(broadcaster: Controller<String>) {
     let y_b = Scalar::random(&mut rand::thread_rng());
     let Y_b = schnorr.encryption_key_for(&y_b);
 
+    let y_a_bytes_arr: [u8; 32] = y_a.to_bytes();
+    let Y_a_bytes_arr: [u8; 33] = Y_a.to_bytes();
+
+    let y_b_bytes_arr: [u8; 32] = y_b.to_bytes();
+    let Y_b_bytes_arr: [u8; 33] = Y_b.to_bytes();
+
     // we wait for registration of Alice & Bob
     thread::sleep(time::Duration::from_secs(1));
 
-    // Broadcast statements to Alice & Bob
-    let message_Ya = format!("Y_a: {}", Y_a);
-    let message_Yb = format!("Y_b: {}", Y_b);
-    broadcaster.broadcast(message_Ya);
-    broadcaster.broadcast(message_Yb);
+    // Print messages from oracle
+    println!("Y_a: {}", Y_a);
+    println!("Y_b: {}\n", Y_b);
+
+    // Broadcast statements as byte array to Alice & Bob
+    broadcaster.broadcast(Y_a_bytes_arr);
+    broadcaster.broadcast(Y_b_bytes_arr);
 
     thread::sleep(time::Duration::from_secs(1));
 
     // Randomly choose y_a or y_b (witness) and send it. That way we effectively choose just one winner.
     let random = rand::thread_rng().gen_range(0..100);
+    let mut bigger_array: [u8; 33] = [0; 33];
     if random < 50 {
-        broadcaster.broadcast(format!("Winning situation for Bob. Witness is y_a: {}", y_a));
+        copy_in_bigger_arr(&y_a_bytes_arr, &mut bigger_array);
+        println!("\nWinning situation for Bob. Witness is y_a: {}", y_a);
     } else {
-        broadcaster.broadcast(format!("Winning situation for Alice. Witness is y_b: {}", y_b));
+        copy_in_bigger_arr(&y_b_bytes_arr, &mut bigger_array);
+        println!("\nWinning situation for Alice. Witness is y_b: {}", y_b);
     }
+    broadcaster.broadcast(bigger_array);
 
     // we wait for listeners to pickup before being dropped
     thread::sleep(time::Duration::from_secs(1));
+}
+
+fn copy_in_bigger_arr(source: &[u8], destination: &mut [u8]) {
+    for i in 0..source.len().min(destination.len()) {
+        destination[i] = source[i];
+    }
 }
